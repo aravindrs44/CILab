5172331913cfb0e3aad1ba2ad68e1ad9c748f599
aravindrs44 <aravindrs@hotmail.com>
2023-06-19 22:12:55 -0700

    changed implementation

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,21 +13,19 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        String upper = myString.toUpperCase();
-        String lower = myString.toLowerCase();
-
-        if (myString.charAt(0) == lower.charAt(0))
-            return true;
-
-        if(myString.equals(upper)) {
+        int count = 0;
+        String string = getString();
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (ch == Character.toUpperCase(ch)) {
+                count++;
+            }
+        }
+        if (count == string.length() || count == 0 || (count == 1 && Character.isUpperCase(string.charAt(0)))) {
             return true;
         }
 
-        if(myString.equals(lower))
-            return true;
-        
         return false;
     }
-
 }
 
